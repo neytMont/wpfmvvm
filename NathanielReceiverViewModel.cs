567d8f2fc11c0297b2f83365006283a906db5216
Nathaniel Monte de Ramos <59034202+neytMont@users.noreply.github.com>
2021-03-09 07:18:36 -0800

    Add files via upload

---
+++ NathanielReceiverViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight; //For mvvmlight 
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;//for class Messenger
+using CECS475_Nathanielwpfmvvm.Messages;
+
+namespace wpfmvvm.ViewModel
+{
+    public class ReceiverViewModel : ViewModelBase
+    {
+        private string _contentText;
+
+        public string ContentText
+        {
+            get { return _contentText; }
+            set
+            {
+                _contentText = value;
+                RaisePropertyChanged("ContentText");
+            }
+        }
+
+        public ReceiverViewModel()
+        {
+            Messenger.Default.Register<NathanielViewModelMessage>(this, OnReceiveMessageAction);
+        }
+
+        private void OnReceiveMessageAction(NathanielViewModelMessage obj)
+        {
+            ContentText = obj.Text;
+        }
+    }
+}
